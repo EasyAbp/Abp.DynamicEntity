fd4557cdedceebcd72ac935663ef4fd44d73dea1
wakuflair <wakuflair@163.com>
2020-08-26 15:26:10 +0800

    add `DynamicSampleMenu`

---
+++ sample/aspnet-core/src/DynamicSample.Web/Menus/DynamicSampleMenus.cs
@@ -0,0 +1,10 @@
+namespace DynamicSample.Web.Menus
+{
+    public class DynamicSampleMenus
+    {
+        private const string Prefix = "DynamicSample";
+
+        //Add your menu items here...
+        //public const string Home = Prefix + ".MyNewMenuItem";
+    }
+}
