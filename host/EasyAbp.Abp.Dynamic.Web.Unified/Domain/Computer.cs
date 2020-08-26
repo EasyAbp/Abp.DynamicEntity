4418cc5b3cd496f038969788389a4fc9b37b4968
wakuflair <wakuflair@163.com>
2020-08-26 13:09:17 +0800

    add `FieldDefinition` and `IDynamicField`

---
+++ host/EasyAbp.Abp.Dynamic.Web.Unified/Domain/Computer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using EasyAbp.Abp.Dynamic.Fields;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace EasyAbp.Abp.Dynamic.Domain
+{
+    public class Computer : FullAuditedAggregateRoot<Guid>, IDynamicField
+    {
+        public Guid FieldDefinitionId { get; }
+        public FieldDefinition FieldDefinition { get; }
+    }
+}
---
+++ src/EasyAbp.Abp.Dynamic.Domain/Fields/FieldDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace EasyAbp.Abp.Dynamic.Fields
+{
+    public class FieldDefinition : FullAuditedAggregateRoot<Guid>, IMultiTenant
+    {
+        public Guid? TenantId { get; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Order { get; set; }
+    }
+}
