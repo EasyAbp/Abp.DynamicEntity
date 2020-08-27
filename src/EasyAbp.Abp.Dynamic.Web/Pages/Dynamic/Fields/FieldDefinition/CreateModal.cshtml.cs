bd13b78250b1dc2d937a7ebb93b68752da9dd55d
wakuflair <wakuflair@163.com>
2020-08-27 17:18:45 +0800

    Adjust namespaces

--- src/EasyAbp.Abp.Dynamic.Web/Pages/Dynamic/Fields/FieldDefinition/CreateModal.cshtml.cs
+++ src/EasyAbp.Abp.Dynamic.Web/Pages/Dynamic/Fields/FieldDefinition/CreateModal.cshtml.cs
@@ -2,7 +2,6 @@ using System.Threading.Tasks;
 using EasyAbp.Abp.Dynamic.FieldDefinitions;
 using EasyAbp.Abp.Dynamic.FieldDefinitions.Dtos;
 using Microsoft.AspNetCore.Mvc;
-using EasyAbp.Abp.Dynamic.Fields;
 using EasyAbp.Abp.Dynamic.Web.Pages.Dynamic.Fields.FieldDefinition.ViewModels;
 
 namespace EasyAbp.Abp.Dynamic.Web.Pages.Dynamic.Fields.FieldDefinition
