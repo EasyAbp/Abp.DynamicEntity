818e38558d899e5c5b7beba22fdaf72e608cea42
wakuflair <wakuflair@163.com>
2020-08-27 13:24:17 +0800

    create `DynamicEntity`

---
+++ src/EasyAbp.Abp.Dynamic.Domain/DynamicEntities/DynamicEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EasyAbp.Abp.Dynamic.ModelDefinitions;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace EasyAbp.Abp.Dynamic.DynamicEntities
+{
+    public class DynamicEntity : FullAuditedAggregateRoot<Guid>, IMultiTenant, IDynamicModel
+    {
+        public Guid? TenantId { get; }
+        public Guid ModelDefinitionId { get; }
+        public ModelDefinition ModelDefinition { get; }
+    }
+}
