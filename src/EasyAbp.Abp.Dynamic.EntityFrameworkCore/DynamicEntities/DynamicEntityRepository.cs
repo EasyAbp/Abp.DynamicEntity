02fbcf98e2fdcf23e8fb502082e1ac8b53067d6a
wakuflair <wakuflair@163.com>
2020-09-16 15:02:37 +0800

    clean usings

--- src/EasyAbp.Abp.Dynamic.EntityFrameworkCore/DynamicEntities/DynamicEntityRepository.cs
+++ src/EasyAbp.Abp.Dynamic.EntityFrameworkCore/DynamicEntities/DynamicEntityRepository.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using EasyAbp.Abp.Dynamic.EntityFrameworkCore;
 using EasyAbp.Abp.Dynamic.ModelDefinitions;
 using Microsoft.EntityFrameworkCore;
-using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
 
