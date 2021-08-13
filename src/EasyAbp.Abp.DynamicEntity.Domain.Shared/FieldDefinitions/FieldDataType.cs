8c6df6a3dba7b20f12a1166c3f9271c0a6de3004
gdlcf88 <gdlcf88@outlook.com>
2021-08-13 15:23:16 +0800

    Remove Flags from FieldDataType.

--- src/EasyAbp.Abp.DynamicEntity.Domain.Shared/FieldDefinitions/FieldDataType.cs
+++ src/EasyAbp.Abp.DynamicEntity.Domain.Shared/FieldDefinitions/FieldDataType.cs
@@ -1,14 +1,11 @@
-﻿using System;
-
-namespace EasyAbp.Abp.DynamicEntity.FieldDefinitions
+﻿namespace EasyAbp.Abp.DynamicEntity.FieldDefinitions
 {
-    [Flags]
     public enum FieldDataType
     {
-        Text = 1,
-        Number = 2,
-        Float = 4,
-        Boolean = 8,
-        DateTime = 16
+        Text,
+        Number,
+        Float,
+        Boolean,
+        DateTime,
     }
 }
