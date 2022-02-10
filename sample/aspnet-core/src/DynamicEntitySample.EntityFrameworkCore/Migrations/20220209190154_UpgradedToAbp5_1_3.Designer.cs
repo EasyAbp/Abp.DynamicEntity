e7a43c186da0acaae9567e5c2616a1e899c26cfb
gdlcf88 <gdlcf88@outlook.com>
2022-02-10 15:13:52 +0800

    Add migrations for samples / demo hosts

--- sample/aspnet-core/src/DynamicEntitySample.EntityFrameworkCore/Migrations/20220209190154_UpgradedToAbp5_1_3.Designer.cs
+++ sample/aspnet-core/src/DynamicEntitySample.EntityFrameworkCore/Migrations/20220209190154_UpgradedToAbp5_1_3.Designer.cs
@@ -4,6 +4,7 @@ using DynamicEntitySample.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace DynamicEntitySample.Migrations
 {
     [DbContext(typeof(DynamicEntitySampleDbContext))]
-    partial class DynamicEntitySampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220209190154_UpgradedToAbp5_1_3")]
+    partial class UpgradedToAbp5_1_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
