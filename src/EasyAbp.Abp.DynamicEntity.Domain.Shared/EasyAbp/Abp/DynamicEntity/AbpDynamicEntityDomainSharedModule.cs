6f79c50b60b45bee6ec4ee047a5b8cf2fbfa49a4
gdlcf88 <gdlcf88@outlook.com>
2023-07-23 23:06:59 +0800

    Upgrade to ABP 7.3.0

--- src/EasyAbp.Abp.DynamicEntity.Domain.Shared/EasyAbp/Abp/DynamicEntity/AbpDynamicEntityDomainSharedModule.cs
+++ src/EasyAbp.Abp.DynamicEntity.Domain.Shared/EasyAbp/Abp/DynamicEntity/AbpDynamicEntityDomainSharedModule.cs
@@ -1,4 +1,5 @@
-﻿using Volo.Abp.Modularity;
+﻿using Volo.Abp.Domain;
+using Volo.Abp.Modularity;
 using Volo.Abp.Localization;
 using EasyAbp.Abp.DynamicEntity.Localization;
 using EasyAbp.Abp.DynamicQuery;
@@ -10,9 +11,10 @@ using Volo.Abp.VirtualFileSystem;
 namespace EasyAbp.Abp.DynamicEntity
 {
     [DependsOn(
-        typeof(AbpValidationModule)
+        typeof(AbpValidationModule),
+        typeof(AbpDddDomainSharedModule),
+        typeof(AbpDynamicQueryDomainSharedModule)
     )]
-    [DependsOn(typeof(AbpDynamicQueryDomainSharedModule))]
     public class AbpDynamicEntityDomainSharedModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
